01fc087eedc24ef9a0b7f3fa722f9d2e0b602362
Shubham Kadaskar <shubhamkadaskar84@gmail.com>
2025-10-11 15:26:48 +0530

    first flow push

---
+++ web/src/main/java/com/shubham/ecom_optimum/web/controller/WebContoller.java
@@ -0,0 +1,18 @@
+package com.shubham.ecom_optimum.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebContoller {
+
+	@GetMapping("/")
+	public String Greet() {
+		return "Hello";
+	}
+	
+	@GetMapping("/status")
+	public String status() {
+		return "Program is ruunnning";
+	}
+}
---
+++ web/src/test/java/com/shubham/ecom_optimum/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.ecom_optimum.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
